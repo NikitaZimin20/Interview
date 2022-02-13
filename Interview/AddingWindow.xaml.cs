5fc35d177c98f49a7518822ff1b509c8653d5dc8
Никита Зимин <m1900757@edu.misis.ru>
2022-02-13 15:20:24 +0300

    Добавьте файлы проекта.

---
+++ Interview/AddingWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Interview
+{
+    /// <summary>
+    /// Логика взаимодействия для AddingWindow.xaml
+    /// </summary>
+    public partial class AddingWindow : Window
+    {
+       private XmlDocument _xd;
+        public AddingWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private string GetLastID()
+        {
+            var xdoc = XDocument.Load("XMLFile1.xml");
+            var lastelement= xdoc.Root.Elements("user").Last();
+            var value = int.Parse(lastelement.Attribute("ID").Value)+1;
+            return value.ToString();
+              
+        }
+        private void AddToXML()
+        {
+            _xd = new XmlDocument();
+            _xd.Load("XMLFile1.xml");
+            XmlNode nl = _xd.SelectSingleNode("users");
+            XmlDocument xd2 = new XmlDocument();
+            xd2.LoadXml("<user ID='"+GetLastID()+"'><name>" + nametxt.Text + "</name><surname>" + surtxt.Text + "</surname><phone>" + phontxt.Text + "</phone></user>");
+            XmlNode n = _xd.ImportNode(xd2.FirstChild, true);
+            nl.AppendChild(n);
+            _xd.Save("XMLFile1.xml");
+        }
+
+       private void Apply_click(object sender, RoutedEventArgs e)
+        {
+            AddToXML();
+        }
+
+        private void Back_click(object sender, MouseButtonEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            main.Show();
+            this.Close();
+        }
+    }
+}
---
+++ Interview/MainWindow.xaml.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Interview
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<CustomArray> user;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+        private void LoadFromXml()
+        {
+            user = new List<CustomArray>();
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("XMLFile1.xml");
+            XmlElement? xRoot = xDoc.DocumentElement;
+
+            foreach (XmlElement xnode in xRoot)
+            {
+                user.Add(new CustomArray { ID=xnode.Attributes.GetNamedItem("ID").Value,Name = xnode.ChildNodes.Item(0).InnerText.Trim(), Surname = xnode.ChildNodes.Item(1).InnerText.Trim(), Phone = xnode.ChildNodes.Item(2).InnerText.Trim() });
+            }
+
+        }
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            LoadFromXml();
+            
+            foreach (var item in user)
+            {             
+                    box.Items.Add(item);             
+            }           
+        }
+       
+
+        private void Person_click(object sender, RoutedEventArgs e)
+        {
+            AddingWindow window = new AddingWindow();
+            window.Show();
+            this.Hide();
+        }
+
+        private void DeleteFromXml(string id)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            xDoc.Load("XMLFile1.xml");
+            XmlElement? xRoot = xDoc.DocumentElement;
+            foreach (XmlElement item in xRoot)
+            {
+                if (item.Attributes.GetNamedItem("ID").Value==id)
+                {
+                    xRoot.RemoveChild(item);
+                }
+            }
+            xDoc.Save("XMLFile1.xml");
+            
+        }
+       
+        private  void trash_click(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                var information = box.SelectedItem;
+                string id = user[box.SelectedIndex].ID;
+                box.Items.Remove(information);
+
+                DeleteFromXml(id);
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("Choose element");
+            }
+            
+        }
+
+        private void Exit_click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
